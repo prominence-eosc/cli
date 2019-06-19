648fe05dd2c5ab04b7c649946ecae88fb705f740
Andrew Lahiff <andrew.lahiff@gmail.com>
2019-06-19 21:17:13 +0000

    Updated version

--- prominence/__init__.py
+++ prominence/__init__.py
@@ -1,6 +1,3 @@
-from prominence.job import ProminenceJob
-from prominence.task import ProminenceTask
-from prominence.workflow import ProminenceWorkflow
 from prominence.client import ProminenceClient
 
-__version__ = '0.1.8'
+__version__ = '0.1.9'
