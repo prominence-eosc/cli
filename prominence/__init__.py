534c547b966473104124c4eb1f50bb2cfe4dba5d
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-07-04 11:40:55 +0100

    Add InputFile

--- prominence/__init__.py
+++ prominence/__init__.py
@@ -2,6 +2,7 @@ from prominence.client import ProminenceClient
 from prominence.resources import Resources
 from prominence.policies import JobPolicies
 from prominence.task import Task
+from prominence.inputfile import InputFile
 from prominence.job import Job
 from prominence.notification import Notification
 
