7d2ae4bd989137dd3f0c54f4c4a0e2c731fc4f84
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-07-05 19:57:31 +0100

    Add workflow policies & workflow

--- prominence/__init__.py
+++ prominence/__init__.py
@@ -1,10 +1,11 @@
 from prominence.client import ProminenceClient
 from prominence.resources import Resources
-from prominence.policies import JobPolicies
+from prominence.policies import JobPolicies, WorkflowPolicies
 from prominence.task import Task
 from prominence.inputfile import InputFile
 from prominence.artifact import Artifact
 from prominence.job import Job
+from prominence.workflow import Workflow
 from prominence.notification import Notification
 
 __version__ = '0.18.0'
