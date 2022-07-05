d2e0142950591cc3c1ecb6a4dba54d9081e7933c
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-07-05 13:49:02 +0100

    Support input file from a string

--- prominence/inputfile.py
+++ prominence/inputfile.py
@@ -5,15 +5,20 @@ class InputFile(object):
     """
     Input file
     """
-    def __init__(self, filename):
+    def __init__(self, filename, content=None):
         self._input_file = filename
+        self._content = content
 
     def json(self):
         """
         JSON representation
         """
-        with open(self._input_file, 'rb') as fh:
+        if not self._content:
+            with open(self._input_file, 'rb') as fh:
+                data = {'filename': os.path.basename(self._input_file),
+                        'content': base64.b64encode(fh.read()).decode("utf-8")}
+        else:
             data = {'filename': os.path.basename(self._input_file),
-                    'content': base64.b64encode(fh.read()).decode("utf-8")}
-            return data
+                    'content': base64.b64encode(bytes(self._content, "utf-8")).decode("utf-8")}
 
+        return data
