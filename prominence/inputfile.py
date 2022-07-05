cced087a7142ec47c3198dcfac3a5b4561329f04
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-07-05 06:22:38 +0100

    Filename is compulsory

--- prominence/inputfile.py
+++ prominence/inputfile.py
@@ -5,16 +5,13 @@ class InputFile(object):
     """
     Input file
     """
-    def __init__(self, filename=None):
+    def __init__(self, filename):
         self._input_file = filename
 
     def json(self):
         """
         JSON representation
         """
-        if not self._input_file:
-            return None
-
         with open(self._input_file, 'rb') as fh:
             data = {'filename': os.path.basename(self._input_file),
                     'content': base64.b64encode(fh.read()).decode("utf-8")}
